9c25b3c28fa6cbdc2572a670ab418fc6a4d9dce3
Luigi <luiginigrelli@me.com>
2019-02-20 18:55:20 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,14 @@ app.post('/todos', (req, res) => {
     })
 })
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos })
+    }, (e) => {
+        res.status(400).send(e)
+    })
+})
+
 /* var newTodo = new Todo({
     text: 'Feed the cat',
     completed: true,
